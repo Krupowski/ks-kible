1e3d9bf889d85b9bed70ff9aa90cf0930836ff27
Krupowski <fqbixirys@gmail.com>
2024-10-13 20:43:36 +0200

    server.lua

---
+++ server.lua
@@ -0,0 +1,9 @@
+ESX.RegisterUsableItem('toiletpaper', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer.getInventoryItem('toiletpaper').count >= 1 then
+        xPlayer.removeInventoryItem('toiletpaper', 1)
+        TriggerClientEvent('esx_toilet:useToilet', source)
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Nie masz papieru toaletowego!')
+    end
+end)
